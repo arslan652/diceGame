bb63b9c1adfe1d2db8d3264c3240e4f6f4806a5f
Arslan Arshad <arshadarslan35@gmail.com>
2023-09-18 17:43:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var randomNumber1 = Math.floor(Math.random()*6) + 1;
 
-var randomDiceImage = "Dice"+ randomNumber1 + ".png";
+var randomDiceImage = "dice"+ randomNumber1 + ".png";
 
 var randomImageSource = "images/" + randomDiceImage;
 
@@ -9,7 +9,7 @@ image1.setAttribute("src",randomImageSource);
 
 
 var randomNumber2 = Math.floor(Math.random()*6)+1;
-var randomImageSource2 = "images/Dice"+ randomNumber2+ ".png";
+var randomImageSource2 = "images/dice"+ randomNumber2+ ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 // var image2 = document.querySelectorAll("img")[1];
